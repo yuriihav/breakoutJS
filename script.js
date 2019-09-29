53b2170bb172b0ace34d96771ef53f20816b9870
Yurii Havrusiev <yuriiha@softwareplanetgroup.com>
2019-09-30 00:34:59 +0300

    Added function, that draws a ball on a canvas and moves it. But ball leaves a trail.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 var canvas = document.getElementById('myCanv');
 
 var ctx = canvas.getContext("2d");
+
+var x_ball, y_ball, radius;
+
+x_ball = 240; y_ball = 160; radius = 20;
+dx = 5;
+
+
+function draw() {
+	//canvas.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.beginPath();
+	//ctx.rect(20, 40, 50, 50);
+	ctx.arc(x_ball, y_ball, radius, 0, Math.PI * 2);
+	ctx.fillStyle = "#F25A29";
+	ctx.fill();
+	ctx.closePath();
+	x_ball += dx;
+}
+
+setInterval(draw, 10);
